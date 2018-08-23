043a25f07a26aad8db5a8e2896e5ab9aa810927c
Simon Johansson <simon@simonjohansson.com>
2018-08-23 16:50:04 +0200

    Update manifest datastructure to be latest one from 6.38

--- manifest/types.go
+++ manifest/types.go
@@ -10,28 +10,30 @@ type Manifest struct {
 }
 
 type Application struct {
-	Name                    string             `yaml:"name,omitempty"`
-	Buildpack               string             `yaml:"buildpack,omitempty"`
-	Command                 string             `yaml:"command,omitempty"`
-	DeprecatedDomain        interface{}        `yaml:"domain,omitempty"`
-	DeprecatedDomains       interface{}        `yaml:"domains,omitempty"`
-	DeprecatedHost          interface{}        `yaml:"host,omitempty"`
-	DeprecatedHosts         interface{}        `yaml:"hosts,omitempty"`
-	DeprecatedNoHostname    interface{}        `yaml:"no-hostname,omitempty"`
-	DiskQuota               string             `yaml:"disk_quota,omitempty"`
-	Docker                  DockerInfo      `yaml:"docker,omitempty"`
-	EnvironmentVariables    map[string]string  `yaml:"env,omitempty"`
-	HealthCheckHTTPEndpoint string             `yaml:"health-check-http-endpoint,omitempty"`
-	HealthCheckType         string             `yaml:"health-check-type,omitempty"`
+	Name                    string            `yaml:"name,omitempty"`
+	Buildpack               string            `yaml:"buildpack,omitempty"`
+	Buildpacks              []string          `yaml:"buildpacks,omitempty"`
+	Command                 string            `yaml:"command,omitempty"`
+	DeprecatedDomain        interface{}       `yaml:"domain,omitempty"`
+	DeprecatedDomains       interface{}       `yaml:"domains,omitempty"`
+	DeprecatedHost          interface{}       `yaml:"host,omitempty"`
+	DeprecatedHosts         interface{}       `yaml:"hosts,omitempty"`
+	DeprecatedNoHostname    interface{}       `yaml:"no-hostname,omitempty"`
+	DiskQuota               string            `yaml:"disk_quota,omitempty"`
+	Docker                  DockerInfo        `yaml:"docker,omitempty"`
+	DropletPath             string            `yaml:"droplet-path,omitempty"`
+	EnvironmentVariables    map[string]string `yaml:"env,omitempty"`
+	HealthCheckHTTPEndpoint string            `yaml:"health-check-http-endpoint,omitempty"`
+	HealthCheckType         string            `yaml:"health-check-type,omitempty"`
 	Instances               int               `yaml:"instances,omitempty"`
-	Memory                  string             `yaml:"memory,omitempty"`
-	NoRoute                 bool               `yaml:"no-route,omitempty"`
-	Path                    string             `yaml:"path,omitempty"`
-	RandomRoute             bool               `yaml:"random-route,omitempty"`
-	Routes                  []Route `yaml:"routes,omitempty"`
-	Services                []string           `yaml:"services,omitempty"`
-	StackName               string             `yaml:"stack,omitempty"`
-	Timeout                 int                `yaml:"timeout,omitempty"`
+	Memory                  string            `yaml:"memory,omitempty"`
+	NoRoute                 bool              `yaml:"no-route,omitempty"`
+	Path                    string            `yaml:"path,omitempty"`
+	RandomRoute             bool              `yaml:"random-route,omitempty"`
+	Routes                  []Route           `yaml:"routes,omitempty"`
+	Services                []string          `yaml:"services,omitempty"`
+	StackName               string            `yaml:"stack,omitempty"`
+	Timeout                 int               `yaml:"timeout,omitempty"`
 }
 
 type Route struct {
@@ -42,4 +44,3 @@ type DockerInfo struct {
 	Image    string `yaml:"image,omitempty"`
 	Username string `yaml:"username,omitempty"`
 }
-
